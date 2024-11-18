7536d490b52978c596f59e4136a0712e444e7165
adrianoff <adrianov87@gmail.com>
2024-11-18 23:15:38 +0300

    In progress

--- epd7in5_V2/epd7in5_V2.ino
+++ epd7in5_V2/epd7in5_V2.ino
@@ -14,6 +14,24 @@ HttpClient client = HttpClient(wifi, serverAddress, port);
 int status = WL_IDLE_STATUS;
 
 void setup() {
+  /*********************/
+    // Serial.begin(9600);
+    // char url_string[256];
+    // String resp;
+
+    // String sessionId;
+    // char sessionIdBuf[6] = "fsddfs";
+
+    // sessionId = "asdfasd";
+
+    // Serial.println("YTEYS");
+    // sprintf(url_string, "/pixels?offset=%lu&limit=%d&session_id=%6d", 1, 2, sessionIdBuf);
+    // Serial.println(sessionId);
+    // Serial.println(url_string);
+    // return;
+  /*********************/
+
+
   Serial.begin(9600);
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to Network named: ");
@@ -22,7 +40,7 @@ void setup() {
     status = WiFi.begin(ssid, pass);
   }
 
-  Serial.print("CONNECTED");
+  Serial.println("CONNECTED");
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
@@ -33,6 +51,10 @@ void setup() {
 
 
 void loop() {  
+    /*********/
+    delay(10000);
+    //return;
+    /*********/
     Epd epd;
     
     Serial.println("e-Paper init...");
@@ -48,13 +70,30 @@ void loop() {
     unsigned long limit = 1000;
     char url_string[256];
     String resp;
+
+    String sessionId;
+    char sessionIdBuf[33];
+
     char base_str[] = "0x00";
+
+    Serial.println("Getting Session");
+    client.get("/start_session");
+    int statusCode = client.responseStatusCode();
+    client.skipResponseHeaders();
+    sessionId = client.responseBody();
+    
+    Serial.println("SessionId:");
+    Serial.println(sessionId);
+
+    sessionId.getBytes(sessionIdBuf, 33);
+    Serial.println(sessionIdBuf);
 
     epd.SendCommand(0x13);
     for(unsigned long i=0; i<96; i++) {
       offset = i*limit;
 
-      sprintf(url_string, "/data?offset=%lu&limit=%d", offset, limit);
+      sprintf(url_string, "/pixels?offset=%lu&limit=%d&session_id=", offset, limit);
+      strcat(url_string, sessionIdBuf);
       Serial.println(offset);
       Serial.println(url_string);
       
